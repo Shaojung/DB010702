840cfe02caae7cf4221d20bb34fb7575465d6aa1
shaojung <shaojung@gmail.com>
2017-01-07 15:23:24 +0800

    2 Line spinner

--- app/src/main/java/com/example/student/db010702/MainActivity.java
+++ app/src/main/java/com/example/student/db010702/MainActivity.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
-    Spinner spinner, spinner2;
+    Spinner spinner, spinner2, spinner3;
     TextView tv, tv2;
     String[] fruits = {"蘋果","橘子", "柳丁", "香蕉"};
     @Override
@@ -41,7 +46,27 @@ public class MainActivity extends AppCompatActivity {
                                 android.R.layout.simple_spinner_item,
                                 fruits);
         spinner2.setAdapter(adapter);
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
+        ArrayList<Map<String, String>> mylist = new ArrayList<>();
+        HashMap<String, String> m1 = new HashMap<>();
+        m1.put("city", "台北");
+        m1.put("code", "02");
+        mylist.add(m1);
+        HashMap<String, String> m2 = new HashMap<>();
+        m2.put("city", "台中");
+        m2.put("code", "04");
+        mylist.add(m2);
+        HashMap<String, String> m3 = new HashMap<>();
+        m3.put("city", "高雄");
+        m3.put("code", "07");
+        mylist.add(m3);
 
+        SimpleAdapter sa = new SimpleAdapter(this,
+                        mylist,
+                        android.R.layout.simple_list_item_2,
+                        new String[] {"city", "code"},
+                        new int[] {android.R.id.text1, android.R.id.text2});
+        spinner3.setAdapter(sa);
 
 
     }
