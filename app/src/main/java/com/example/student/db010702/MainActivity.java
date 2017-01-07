1f37057db2eba5719c0ca956ccd6126e51f26348
shaojung <shaojung@gmail.com>
2017-01-07 14:24:56 +0800

    setOnItemSelectedListener

--- app/src/main/java/com/example/student/db010702/MainActivity.java
+++ app/src/main/java/com/example/student/db010702/MainActivity.java
@@ -9,13 +9,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Spinner spinner;
-    TextView tv;
+    TextView tv, tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.textView);
-
+        tv2 = (TextView) findViewById(R.id.textView2);
         spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -31,8 +31,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
+    }
+    public void click1(View v)
+    {
+        int p = spinner.getSelectedItemPosition();
+        String data[] = getResources().getStringArray(R.array.cities);
+        tv2.setText(data[p]);
     }
 }
